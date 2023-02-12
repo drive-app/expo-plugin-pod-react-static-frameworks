7bbdded9f1ba980aac76596d25cf57519b92901d
Alexander Collins <alexcollins010@gmail.com>
2023-02-12 13:16:08 +1100

    fixing injectStaticNativePackagesMutation to mutate pod file as required

--- index.js
+++ index.js
@@ -26,13 +26,17 @@ const injectStaticNativePackagesMutation = (src) => {
   console.log("> use_frameworks! in source");
 
   const lines = src.split(/\r?\n/);
-  let index = lines.findIndex((line) => line.startsWith(`  use_frameworks!`));
+  let index = lines.findIndex((line) =>
+    line.startsWith(
+      `  use_frameworks! :linkage => podfile_properties['ios.useFrameworks'].to_sym if podfile_properties['ios.useFrameworks']`
+    )
+  );
   let tempL = lines[index];
 
   const injection =
-    "\tpre_install do |installer|\n\t\tinstaller.pod_targets.each do |pod|\n\t\t\tif !pod.name.start_width?('react-native-shake') || pod.name.start_with?('react-native-') || pod.name.start_with?('ReactNative') || pod.name.start_with?('RN') || pod.name.eql?('RNPermissions') || pod.name.start_with?('Permission-')\n\t\t\t\tdef pod.build_type;\n\t\t\t\t\tPod::BuildType.static_library\n\t\t\t\tend\n\t\t\tend\n\t\tend\n\tend";
+    "\tuse_frameworks!\n\tpre_install do |installer|\n\t\tinstaller.pod_targets.each do |pod|\n\t\t\tif pod.name.start_with?('react-native-') || pod.name.start_with?('ReactNative') || pod.name.start_with?('RN') || pod.name.eql?('RNPermissions') || pod.name.start_with?('Permission-')\n\t\t\t\tdef pod.build_type;\n\t\t\t\t\tPod::BuildType.static_library\n\t\t\t\tend\n\t\t\tend\n\t\tend\n\tend";
 
-  return src.replace(tempL, tempL + "\n" + injection);
+  return src.replace(tempL, injection);
 };
 
 module.exports = (config) =>
